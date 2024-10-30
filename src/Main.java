aa95f138a11ed651b8b2ca5bef921f4490e75d1e
BienKieu <bien14112005@gmail.com>
2024-10-30 20:34:11 +0700

    git push

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.print("Hello and welcome!");
+        for(int i = 0; i < 10; ++i)
+            System.out.println(i);
     }
 }
