e463de5bd2d550e53dc56299fec593a138068390
nguyenhuykiet <nguyenhuykiet8@gmail.com>
2024-10-30 21:38:20 +0700

    git push

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.print("Hello and welcome!");
-    }
+	public static void main(String[] args) {
+		System.out.print("Hello and welcome!");
+
+        System.out.println("NHK5131");
+	}
 }
