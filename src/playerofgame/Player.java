301825f6c2f2eab534802481422ef5805d4433d6
Nehut141 <tuyendznha01gmail.com>
2024-11-01 23:11:49 +0700

    git push

---
+++ src/playerofgame/Player.java
@@ -0,0 +1,51 @@
+package playerofgame;
+
+import deckofcards.Card;
+
+import java.util.ArrayList;
+
+public class Player {
+    private String nameOfPlayer;
+    private int scoreOfPlayer = 0;
+    private ArrayList<Card> cardOfPlayer  = new ArrayList<Card>();
+
+    public Player() {
+    }
+
+    public Player(String nameOfPlayer) {
+        this.nameOfPlayer = nameOfPlayer;
+    }
+
+    public String getNameOfPlayer() {
+        return nameOfPlayer;
+    }
+
+    public void setNameOfPlayer(String nameOfPlayer) {
+        this.nameOfPlayer = nameOfPlayer;
+    }
+
+    public int getScoreOfPlayer() {
+        return scoreOfPlayer;
+    }
+
+    public void addScoreOfPlayer(int rankOfCard) {
+        this.scoreOfPlayer +=rankOfCard;
+    }
+
+    public ArrayList<Card> getCardOfPlayer() {
+        return cardOfPlayer;
+    }
+
+    public void addCardOfPlayer(Card card) {
+        cardOfPlayer.add(card);
+        addScoreOfPlayer(card.getRank());
+    }
+
+    @Override
+    public String toString() {
+        return "nameOfPlayer='" + nameOfPlayer + '\'' +
+                ", scoreOfPlayer=" + scoreOfPlayer + "\n" +
+                ", cardOfPlayer=" + cardOfPlayer +
+                '}' + "\n";
+    }
+}
